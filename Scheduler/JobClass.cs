649911d4f8a3f91669c5814a8ac5b7974b7afe3c
Dan Garcia <dgarcia1@umassd.edu>
2015-11-16 11:30:09 -0500

    Added class

---
+++ Scheduler/JobClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Scheduler
+{
+	public class Job
+	{
+		public int pid;
+		public int cpu_burst;
+		public int io_burst;
+		public Job ()
+		{
+			pid = 0;
+			cpu_burst = 0;
+			io_burst = 0;
+		}
+	}
+}
+
